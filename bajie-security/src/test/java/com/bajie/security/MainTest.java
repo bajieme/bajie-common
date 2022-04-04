aff1dcc1bf12f916e058c5b5977ba70ebc4a5e30
bajie <cjz@servyou.com.cn>
2022-04-04 14:17:38 +0800

    :bento: 添加security工程

---
+++ bajie-security/src/test/java/com/bajie/security/MainTest.java
@@ -0,0 +1,16 @@
+package com.bajie.security;
+
+
+/**
+ * 测试
+ *
+ * @author bajie
+ * @date 2022-04-04 2:15 下午
+ * @since 1.0.0
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("------------------------  ------------------------");
+    }
+}
